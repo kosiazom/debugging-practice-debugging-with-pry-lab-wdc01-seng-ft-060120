4f1271049ce2ada8e44d55f5813ca9e276f93a37
Kosi Akporji <kosiazom@gmail.com>
2020-06-03 13:24:24 -0400

    first commit

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,15 @@
 require 'pry'
+player1 = "Daisy"
+player2 = "Benjamin Button"
 
 def start_game(player1, player2)
-  "Hello #{player1} & #{playar2}"
+puts "Hello #{player1} & #{player2}"
 end
+start_game(player1, player2)
 
-def play_game(player2)
-  "#{player1} is better than #{player2}"
+
+def play_game(player1, player2)
+  puts "#{player1} is better than #{player2}"
+  
 end
+play_game(player1, player2)
